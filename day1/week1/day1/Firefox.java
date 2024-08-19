bcb7133680d7c3d2a1ac7f46b93fdb9a49ea1f3b
santhoshadith <avrrohith@gmail.com>
2024-08-19 22:28:26 +0530

    Add files via upload

---
+++ day1/week1/day1/Firefox.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 100.2f;
+		System.out.println("The Browser Version is "+browserVersion);
+		String browserName = "firefox";
+		System.out.println("The Browser Name is "+browserName);
+		Boolean isVisible = true;
+		System.out.println("The Visibility is "+isVisible);
+		 int releaseYear = 1998;
+		  System.out.println("The releaseYear is "+releaseYear); 
+		  char browserLogo = 'f'; 
+		  System.out.println("The Browser logo is "+browserLogo);
+		 
+		
+		System.out.println("The Browser Version is ,"+browserVersion+"\n"+browserName+"\n");
+		
+		
+		
+		
+
+	}
+
+}
---
+++ day1/week1/day2/PrintDuplicates.java
@@ -0,0 +1,26 @@
+package week1.day2;
+
+public class PrintDuplicates {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]num= {2,5,7,7,5,9,2,3};
+		for (int i = 0; i < num.length; i++) {
+			for (int j = i+1; j < num.length; j++) {
+				if(num[i] ==num[j])
+				{
+				  System.out.println(num[i]);
+				}
+				  else 
+					  continue;
+				}
+				
+				
+			}
+			
+		}
+			
+
+	}
+
+
